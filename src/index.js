985f0802b90f76d00c6e5ef8083f800a6db9b52a
Alex859R <repetzky.a@icloud.com>
2020-11-26 18:15:06 +0300

    refactor state to oop store

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-import renderTree from "./render";
-import state from "./redux/state";
+import React from 'react';
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import store from "./redux/state";
 
-renderTree(state);
+const renderTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={ state } addPost={ store.addPost.bind(store) } updatePostText={ store.updatePostText.bind(store) }/>
+        </BrowserRouter>,
+        document.getElementById('root')
+    );
+}
+
+store.subscribe(renderTree);
+
+renderTree(store.state);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,82 +1,92 @@
-import renderTree from "../render";
-const state = {
-    profilePage: {
-        newPostText: '',
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likesCount: 12},
-            {id: 2, message: 'It\'s my first post', likesCount: 11},
-            {id: 3, message: 'Blabla', likesCount: 11},
-            {id: 4, message: 'Dada', likesCount: 11}
-        ],
+const store = {
+    _state: {
+        profilePage: {
+            newPostText: 'Hello my little friends',
+            posts: [
+                {id: 1, message: 'Hi, how are you?', likesCount: 12},
+                {id: 2, message: 'It\'s my first post', likesCount: 11},
+                {id: 3, message: 'Blabla', likesCount: 11},
+                {id: 4, message: 'Dada', likesCount: 11}
+            ],
+        },
+        dialogsPage: {
+            dialogs: [
+                {
+                    id: 1,
+                    name: 'Dimych'
+                },
+                {
+                    id: 2,
+                    name: 'Andrew'
+                },
+                {
+                    id: 3,
+                    name: 'Sveta'
+                },
+                {
+                    id: 4,
+                    name: 'Sasha'
+                },
+                {
+                    id: 5,
+                    name: 'Viktor'
+                },
+                {
+                    id: 6,
+                    name: 'Valera'
+                }
+            ],
+            messages: [
+                {
+                    id: 1,
+                    message: 'Hi'
+                },
+                {
+                    id: 2,
+                    message: 'How is your it-kamasutra?'
+                },
+                {
+                    id: 3,
+                    message: 'Yo'
+                },
+                {
+                    id: 4,
+                    message: 'Yo'
+                },
+                {
+                    id: 5,
+                    message: 'Yo'
+                }
+            ],
+        },
     },
-    dialogsPage: {
-        dialogs: [
-            {
-                id: 1,
-                name: 'Dimych'
-            },
-            {
-                id: 2,
-                name: 'Andrew'
-            },
-            {
-                id: 3,
-                name: 'Sveta'
-            },
-            {
-                id: 4,
-                name: 'Sasha'
-            },
-            {
-                id: 5,
-                name: 'Viktor'
-            },
-            {
-                id: 6,
-                name: 'Valera'
-            }
-        ],
-        messages: [
-            {
-                id: 1,
-                message: 'Hi'
-            },
-            {
-                id: 2,
-                message: 'How is your it-kamasutra?'
-            },
-            {
-                id: 3,
-                message: 'Yo'
-            },
-            {
-                id: 4,
-                message: 'Yo'
-            },
-            {
-                id: 5,
-                message: 'Yo'
-            }
-        ],
+
+    _callSubscriber() {},
+
+    get state() {
+        return this._state;
     },
+
+    updatePostText(text) {
+        this.state.profilePage.newPostText = text;
+        this._callSubscriber(this.state);
+    },
+
+    addPost() {
+        const newPost = {
+            id: 5,
+            message: this.state.profilePage.newPostText,
+            likesCount: 0
+        };
+
+        this.state.profilePage.posts.push(newPost);
+        this.state.profilePage.newPostText = "";
+        this._callSubscriber(this.state);
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    }
 }
 
-export const updatePostText = (text) => {
-    state.profilePage.newPostText = text;
-    renderTree(state);
-}
-
-export const addPost = () => {
-    const newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = "";
-    renderTree(state);
-}
-
-window.state = state;
-export default state;
+export default store;
