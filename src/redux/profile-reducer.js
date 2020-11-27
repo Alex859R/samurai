829b5760710c1940d4551348ef94ea731ce83325
Alex859R <repetzky.a@icloud.com>
2020-11-27 18:08:02 +0300

    inculcation redux

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,17 @@
 const ADD_POST = "ADD-POST";
 const UPDATE_POST = "UPDATE-POST";
 
-const profileReducer = (state, action) => {
+const initialState = {
+    newPostText: '',
+    posts: [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'It\'s my first post', likesCount: 11},
+        {id: 3, message: 'Blabla', likesCount: 11},
+        {id: 4, message: 'Dada', likesCount: 11}
+    ],
+};
+
+const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST:
             const newPost = {
--- src/redux/store.js
+++ src/redux/store.js
@@ -6,26 +6,10 @@ const store = {
         profilePage: {
             newPostText: '',
             posts: [
-                {
-                    id: 1,
-                    message: 'Hi, how are you?',
-                    likesCount: 12
-                },
-                {
-                    id: 2,
-                    message: 'It\'s my first post',
-                    likesCount: 11
-                },
-                {
-                    id: 3,
-                    message: 'Blabla',
-                    likesCount: 11
-                },
-                {
-                    id: 4,
-                    message: 'Dada',
-                    likesCount: 11
-                }
+                { id: 1, message: 'Hi, how are you?', likesCount: 12 },
+                { id: 2, message: 'It\'s my first post', likesCount: 11 },
+                { id: 3, message: 'Blabla', likesCount: 11 },
+                { id: 4, message: 'Dada', likesCount: 11 }
             ],
         },
         dialogsPage: {
